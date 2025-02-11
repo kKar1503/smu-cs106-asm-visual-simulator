6aec063abf8d62dd7f37871b0ab523861559ea4b
Yam Kar Lok <94188697+kKar1503@users.noreply.github.com>
2025-02-11 23:00:14 +0800

    feat: Configs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
+    reactStrictMode: true,
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    output: "export",
+    devIndicators: {
+        appIsrStatus: false,
+    },
 };
 
 export default nextConfig;
