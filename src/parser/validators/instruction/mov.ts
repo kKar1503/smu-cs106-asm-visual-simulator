f05651eb9d0d59b78b7c7eccf152299f7a687ec0
Yam Kar Lok <94188697+kKar1503@users.noreply.github.com>
2025-02-17 23:57:11 +0800

    feat: Added operands validator for mov validation schema

--- src/parser/validators/instruction/mov.ts
+++ src/parser/validators/instruction/mov.ts
@@ -1,12 +1,23 @@
 import { SUPPORTED_VARIANTS } from "@/interpreter/variant";
 import { InstructionValidationSchema } from "@/parser/validator";
+import absqOperandsValidator from "../absq_operands";
+import movExtensionOperandsValidator from "../mov_extension_operands";
+import noMemoryToMemoryValidator from "../no_memory_to_memory_operands";
+import validMemoryOperandsValidator from "../valid_memory_operands";
+import variantRegisterOperandSizeValidator from "../variant_size_operands";
 
 const validator: InstructionValidationSchema = {
     instruction: "MOV",
     supportedVariants: [...SUPPORTED_VARIANTS],
     operand: {
         counts: [2],
-        validators: [],
+        validators: [
+            absqOperandsValidator,
+            movExtensionOperandsValidator,
+            noMemoryToMemoryValidator,
+            validMemoryOperandsValidator,
+            variantRegisterOperandSizeValidator,
+        ],
     },
     validators: [],
 };
