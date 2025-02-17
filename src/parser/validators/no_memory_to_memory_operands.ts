f98c4c371a5d0b6d1bdf0f11c9016c1453e3db0a
Yam Kar Lok <94188697+kKar1503@users.noreply.github.com>
2025-02-17 23:27:25 +0800

    wip: No memory to memory operands

---
+++ src/parser/validators/no_memory_to_memory_operands.ts
@@ -0,0 +1,8 @@
+import { OperandValidator } from "../validator";
+
+const noMemoryToMemoryValidator: OperandValidator = function (instructionVariant, operands) {
+    //TODO: Implement noMemoryToMemoryValidator
+    return null;
+};
+
+export default noMemoryToMemoryValidator;
