7ba62615639c027360a6ca8dc900b8111d7f6501
Yam Kar Lok <94188697+kKar1503@users.noreply.github.com>
2025-02-18 16:53:41 +0800

    feat: Added schema for unary operations

---
+++ src/parser/validators/instruction/not.ts
@@ -0,0 +1,21 @@
+import { SIZE_VARIANTS } from "@/lexer/variant";
+import { InstructionValidationSchema } from "@/parser/validator";
+import validMemoryOperandsValidator from "../valid_memory_operands";
+import validCombinationsOperandsValidator from "../valid_combinations_operands";
+import { TokenType } from "@/lexer/lexer";
+import variantRegisterSizeOperandsValidator from "../variant_register_size_operands";
+import variantMemoryDisplacementSizeOperandsValidator from "../variant_memory_displacement_size_operands";
+
+const validator: InstructionValidationSchema = {
+    instruction: "NOT",
+    supportedVariants: [...SIZE_VARIANTS],
+    operandCounts: [1],
+    validators: [
+        validMemoryOperandsValidator,
+        validCombinationsOperandsValidator([TokenType.REGISTER], [TokenType.MEMORY]),
+        variantRegisterSizeOperandsValidator,
+        variantMemoryDisplacementSizeOperandsValidator,
+    ],
+};
+
+export default validator;
